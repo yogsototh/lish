dacd2b692f879193f50ba7fd0e0654feb66322f3
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2017-02-18 19:12:35 +0100

    wip

--- src/Lish/Core.hs
+++ src/Lish/Core.hs
@@ -28,39 +28,78 @@ mainLoop = do
       eval (parseCmd line)
       mainLoop
 
-data Cmd = Cmd String [String] deriving (Eq, Show)
+data SExp = S [SExp]
+          | Str String
+          deriving (Eq, Show)
 
-type Command = [String] -> IO ()
+-- | a Command is a function that takes arguments
+-- and then returns an output that will be a list of lines
+type Command = [String] -> IO [String]
 
-internalCommands :: [(String,Command)]
-internalCommands = [("pr",\args -> putStrLn (intercalate " " args))]
+-- | = PARSE
 
--- PARSE
-parseCmd :: String -> Either ParseError Cmd
-parseCmd = parse sExprParser "S-Expr"
+parseCmd :: String -> Either ParseError SExp
+parseCmd = parse parseExpr "S-Expr"
 
 identifier :: Parsec String () String
 identifier = many1 (noneOf " \t")
 
-sExprParser :: Parsec String () Cmd
-sExprParser = do
-  (cmdname:args) <- many1 (identifier <* spaces)
+parseList :: Parsec String () SExp
+parseList = fmap S $ sepBy parseExpr spaces
+
+parseExpr :: Parsec String () SExp
+parseExpr = do
+  S (cmdname:args) <- between (char '(')
+                              (char ')')
+                              parseList
   return $ Cmd cmdname args
 
--- EVAL
+-- |
+-- = EVAL
+
+-- |
+-- == INTERNAL COMMANDS
+
+prn :: Command
+prn str = do
+  putStrLn (intercalate " " str)
+  return []
+
+pr :: Command
+pr str = do
+  putStr (intercalate " " str)
+  return []
+
+
+internalCommands :: [(String,Command)]
+internalCommands = [("prn",prn)
+                   , ("pr", pr)]
+
 internalFunction :: String -> Maybe Command
 internalFunction cmdname = lookup cmdname internalCommands
 
-execute :: String -> [String] -> IO ()
-execute cmd args = do
+execute :: SExp -> IO [String]
+execute (Cmd cmd args) = do
     res <- createProcess (proc cmd args) { std_out = CreatePipe }
     case res of
-      (_, Just hout, _, _) -> hGetContents hout >>= mapM_ putStrLn . (map ("o-o " ++)) . lines
-      _                    -> putStrLn "no output"
+      (_, Just hout, _, _) -> hGetContents hout >>= return . lines
+      _                    -> putStrLn "no output" >> return []
+execute _ = error "execute not on Cmd! This should never have happened!"
 
-eval :: Either ParseError Cmd -> InputT IO ()
+-- | Evaluate a command line
+eval :: Either ParseError SExp -> InputT IO ()
 eval parsed = case parsed of
-  Right (Cmd cmdname args) -> case internalFunction cmdname of
-                                Just f -> liftIO (f args)
-                                Nothing -> liftIO (execute cmdname args)
+  Right sexp -> liftIO (seval sexp) >>= mapM_ outputStr
   Left err -> outputStrLn (show err)
+
+seval :: SExp -> IO [String]
+seval (cmd@(Cmd cmdname args)) = case internalFunction cmdname of
+                                   Just f -> f args
+                                   Nothing -> execute cmd
+seval (S exprs) = do
+  lsOflines <- mapM seval exprs
+  case map unlines lsOflines of
+    (cmd:args) -> seval (Cmd cmd args)
+    _ -> putStrLn "ERROR in seval, empty S-Expr" >> return []
+
+seval (Str s) = return [s]
